f9a9b9ea305591249a27a8732808d9788e6ee2c1
vinodyadav9 <vinod.yadav@slovink.com>
2025-08-26 13:33:05 +0530

    first commit

---
+++ _example/key-vault-with-access-policy/example.tf
@@ -0,0 +1,112 @@
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group" {
+  source      = "git@github.com:pankajyadavdevops/terraform-azure-resource-group.git?ref=v1.0.2"
+  version     = "1.0.2"
+  name        = "app"
+  environment = "test"
+  location    = "North Europe"
+}
+
+module "vnet" {
+  source                 = "git@github.com:pankajyadavdevops/terraform-azure-vnet.git?ref=v1.0.2"
+  version                = "1.0.2"
+  name                   = "app"
+  environment            = "test"
+  resource_group_name    = module.resource_group.resource_group_name
+  location               = module.resource_group.resource_group_location
+  address_space          = "10.0.0.0/16"
+  enable_ddos_pp         = false
+  enable_network_watcher = false
+}
+
+
+module "subnet" {
+  source               = "git@github.com:pankajyadavdevops/terraform-azure-subnet.git?ref=v1.0.2"
+  version              = "1.0.2"
+  name                 = "app"
+  environment          = "test"
+  resource_group_name  = module.resource_group.resource_group_name
+  location             = module.resource_group.resource_group_location
+  virtual_network_name = join("", module.vnet[*].name)
+
+  #subnet
+  subnet_names    = ["subnet1"]
+  subnet_prefixes = ["10.0.1.0/24"]
+
+  # route_table
+  enable_route_table = true
+  route_table_name   = "default_subnet"
+  routes = [
+    {
+      name           = "rt-test"
+      address_prefix = "0.0.0.0/0"
+      next_hop_type  = "Internet"
+    }
+  ]
+}
+
+#Key Vault
+module "vault" {
+  source                      = "./../.."
+  name                        = "app"
+  environment                 = "test"
+  resource_group_name         = module.resource_group.resource_group_name
+  purge_protection_enabled    = false
+  enabled_for_disk_encryption = true
+  sku_name                    = "standard"
+  subnet_id                   = module.subnet.default_subnet_id
+  virtual_network_id          = module.vnet.id
+  enable_private_endpoint     = true
+
+  depends_on = [module.resource_group, module.vnet]
+  access_policy = [
+    {
+      object_id = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+      key_permissions = [
+        "Get",
+        "List",
+        "Update",
+        "Create",
+        "Import",
+        "Delete",
+        "Recover",
+        "Backup",
+        "Restore",
+        "UnwrapKey",
+        "WrapKey",
+        "GetRotationPolicy"
+      ]
+      certificate_permissions = [
+        "Get",
+        "List",
+        "Update",
+        "Create",
+        "Import",
+        "Delete",
+        "Recover",
+        "Backup",
+        "Restore",
+        "ManageContacts",
+        "ManageIssuers",
+        "GetIssuers",
+        "ListIssuers",
+        "SetIssuers",
+        "DeleteIssuers"
+      ]
+      secret_permissions = [
+        "Get",
+        "List",
+        "Set",
+        "Delete",
+        "Recover",
+        "Backup",
+        "Restore"
+      ]
+      storage_permissions = []
+    },
+  ]
+
+}
